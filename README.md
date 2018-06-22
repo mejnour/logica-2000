6257626051d2fad4d7a89c7ddc8f10e7dc0b45b4
mejnour <apina.dev@gmail.com>
2018-06-21 21:40:56 -0300

    conteudo movido

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# logica-2000
+# Website-for-studying-computational-logic
+
+## Andrei's Razor
+
+This project is a required assignment from UFPB's Formal Languages class. Lectured by [Andrei Formiga](https://github.com/tautologico) :sunglasses:.
+
+Team: [José Arnaldo](https://github.com/mejnour), [Juliano Nunes](https://github.com/jndsgw), [Paulo Henrique](https://github.com/paulohbmatias) e [Renan Lages](https://github.com/kusmin).
+
+### wat is dis
+
+This Website holds Computational Logic-related quizzes and such, as means to help freshmen in Computer Science and IT-like courses challenge themselves.
+
+**Features:**
+- Login/Password access
+  - Student account
+  - Teacher account
+- Twitter-like notifications
+- Teacher-oriented quiz creation
+- Multiple choice quizzes
+
+### Crafting Tools
+
+Mysteriously developed using HTML, CSS, BootStrap and JavaScript.
---
+++ navibar.js
@@ -0,0 +1,29 @@
+document.write('\
+\
+<nav class="navbar" style="background: transparent">\
+\
+    <div class="container">\
+\
+      <div class="navbar-header">\
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#barra-navegacao">\
+          <span class="icon-bar" style="background-color: white;"></span>\
+          <span class="icon-bar" style="background-color: white;"></span>\
+          <span class="icon-bar" style="background-color: white;"></span>\
+        </button>\
+          <a class="navbar-brand" href="intro.html" style="color: white">Lógica 2000</a>\
+      </div>\
+\
+      <div id="barra-navegacao" class="collapse navbar-collapse">\
+\
+        <ul class="nav navbar-nav navbar-right">\
+            <li><a href="registrar.html" style="color: white"><span class="glyphicon glyphicon-user" style="color: white"></span> Registrar-se</a></li>\
+            <li><a href="login.html" style="color: white"><span class="glyphicon glyphicon-log-in" style="color: white"></span> Login</a></li>\
+      </ul>\
+\
+      </div>\
+\
+    </div>\
+\
+</nav>\
+\
+');
